70f6a07b187a115429b23dfc2f9e1b3e7841d61b
Silvia Trejo <s.y.trejo26@gmail.com>
2022-11-14 13:07:42 -0500

    Saving work

---
+++ JAV-20-Mockito-start/src/test/java/gameShowTest/GameShowTest.java
@@ -0,0 +1,60 @@
+package gameShowTest;
+
+
+class GameShowTest {
+	
+	// 1. Write the test
+	// 2. make it compile
+	// 3. watch it fail
+	// 4. make it pass
+	// 5. refactor and generalize
+	
+	
+//Write a test that returns true when the player guesses the correct word 
+		//Arrange -- Given (control) -- setting up the game
+
+		
+		//Act -- when --- play the game
+
+		
+		
+		//Assert --then --- check if you win or lose
+
+//Write a test that returns true when the player guesses incorrectly 3 times
+		//Arrange -- Given (control) -- setting up the game
+		
+		
+		//every time a method call is made to guess return the wrong word.
+		
+		
+		//Act -- when --- play the game / make the method call
+		
+		
+		//Assert --then --- check if you win or lose
+	
+	
+//Write a test that calls the guess method in the person class and inject the correct word
+//verify that the method is called at least once.
+
+		//Arrange
+	
+		
+		//Act
+
+		
+		//Assert
+
+	
+//write a test were the player gets 3 guesses to guess the correct word
+//the player will guess correctly and win.
+
+		//Arrange
+
+		
+		//Act
+
+		
+		//Assert
+	
+
+}
